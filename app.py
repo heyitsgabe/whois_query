fc458d2600579c832788d9f31b7ac5ff98626519
Gabriel Morales <heyitsgabe@users.noreply.github.com>
2022-05-07 16:50:13 -0400

    removed Notes.txt file

--- app.py
+++ app.py
@@ -8,28 +8,16 @@ import requests
 ######################################################
 
 global url
-global response
+global responseglobal 
+global domains_lst
 # full_api_url = https://www.whoisxmlapi.com/whoisserver/WhoisService?apiKey=at_XGGEvybXDmcdBvTnBiNbQyGQekz6i&domainName=google.com
 apiKey = 'at_XGGEvybXDmcdBvTnBiNbQyGQekz6i'
 
-'''
-client = Client(api_key=at_XGGEvybXDmcdBvTnBiNbQyGQekz6i)
-
-#getting parsed whois record as a model instance. 
-whois = client.data('whoisxmlapi.com')
-print(whois.created_date_raw)
-
-# Get raw API response
-resp_str = client.raw_data('whoisxmlapi.com')
-'''
-
-'''
-url = 'https://www.whoisxmlapi.com/whoisserver/WhoisService'
 
 ######################################################
 # Account Details
 ######################################################
-
+'''
 username = 'gmm0812@protonmail.com'
 password = 'who-CV$1012~rA'
 domain = 'whoisxmlapi.com'
@@ -37,27 +25,10 @@ domain = 'whoisxmlapi.com'
 
 
 
-'''
-try:
-    from urllib.request import urlopen
-except ImportError:
-    from urllib2 import urlopen
-
-domainName = 'pfizer.com'
-apiKey = 'at_XGGEvybXDmcdBvTnBiNbQyGQekz6i'
-
-url = 'https://www.whoisxmlapi.com/whoisserver/WhoisService?'\
-    + 'domainName=' + domainName + '&apiKey=' + apiKey + "&outputFormat=JSON"
-
-print(urlopen(url).read().decode('utf8'))
-'''
-
-
-
 ######################################################
 ## parse domains from yml to list
 ######################################################
-global domains_lst
+
 # domains_lst = ['google.com', 'apple.com', 'pfizer.com']
 
 file_list = open('domains.yml')
@@ -65,6 +36,20 @@ domains_lst = []
 for line in file_list:
     domains_lst.append(line.strip())
 print(domains_lst)
+
+
+
+######################################################
+## notify email
+######################################################
+
+## use gmail API for email notifications
+## https://pythongeeks.org/send-email-using-python/
+
+def email_notify():
+
+    print('sent notification')
+
 
 
 ######################################################
@@ -76,23 +61,14 @@ while i < len(domains_lst):
     url = 'https://www.whoisxmlapi.com/whoisserver/WhoisService?' + 'domainName=' + domains_lst[i] + '&apiKey=' + apiKey + "&outputFormat=JSON"
     print(url)    
     response = requests.get(url)
-    print(response.json())
+    # print(response.json())
+
+    ## Writing output to temp file for regex parsing
+    f = open('tmp_'+domains_lst[i]+'_whois.txt', 'w')
+    f.write(str(response.json()))
+    f.close()
     i+=100
 else:
     print('exiting')
 
 
-
-######################################################
-## test get request for connection
-######################################################
-
-# response = requests.get(url)
-# print(response.status_code)
-# print(response.json())
-
-######################################################
-## notify email
-######################################################
-
-## use gmail API for email notifications
